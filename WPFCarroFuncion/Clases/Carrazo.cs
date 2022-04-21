f67415cffd5d89b434a5082333cc47d574fe5caa
HenryCabrera <henrycabrera083@gmail.com>
2022-04-21 16:08:12 -0600

    Agregar archivos de proyecto.

---
+++ WPFCarroFuncion/Clases/Carrazo.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCarroFuncion.Clases
+{
+    internal class Carrazo
+    {
+        public string Marca { get; }
+        public int Modelo { get; }
+        public int cont { get; }
+        public String Color { get; set; }
+
+        public string owner { get; set; }
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+
+        public int MAXVELOCIDAD { get; }
+
+        public Carrazo(string _marca, string _owner)
+        {
+            Marca = _marca;
+            owner = _owner;
+            Encendido = 0;
+            MAXVELOCIDAD = 320;
+            this.velocidad_actual = 0;
+
+        }
+
+
+
+        public int GetVelocidadActual()
+        {
+            return velocidad_actual;
+        }
+
+        public string EncenderMotor()
+        {
+            if (Encendido == 0)
+            {
+
+                Encendido = 1;
+                this.velocidad_actual = 0;
+                return "BRRUUUUMMM, LISTOS PARA CORRER";
+            }
+            else
+            {
+                return "El auto ya estaba encendidooo";
+            }
+        }
+
+        public string acelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"Lo sentimos, pero el carro esta apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += 10;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH Vamos a la velocidad maximaa.";
+            }
+
+            return mensaje;
+        }
+
+        public string desacelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = "No podemos desacelerar, el auto está apagado.";
+                return mensaje;
+            }
+            velocidad_actual -= 15;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Desaceleramos, ahora vamos a: {velocidad_actual}KPH.";
+            }
+            return mensaje;
+        }
+        public string frenar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = "No podemos frenar, el auto está apagado.";
+                return mensaje;
+            }
+            this.velocidad_actual = 0;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Frenamos por completo, estamos a {this.velocidad_actual}KPH";
+            }
+            return mensaje;
+        }
+        public string apagar()
+        {
+            if (Encendido == 0)
+            {
+
+                return "El auto ya estaba apagadoooo.";
+            }
+            else
+            {
+                Encendido = 0;
+                this.velocidad_actual = 0;
+                return "¡Listo!, apagamos el autoo.";
+            }
+
+        }
+    }
+}
---
+++ WPFCarroFuncion/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFCarroFuncion.Clases;
+
+namespace WPFCarroFuncion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Carrazo man = new Carrazo("Hyundai", "Henry");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnEncender_Click(object sender, RoutedEventArgs e)
+        {
+            Encendidomsg.Text = man.EncenderMotor();
+        }
+
+        private void BttnAcelerar_Click(object sender, RoutedEventArgs e)
+        {
+            Acelerarmsg.Text = man.acelerar();
+        }
+
+        private void BttnBajarVel_Click(object sender, RoutedEventArgs e)
+        {
+            Desacelerarmsg.Text = man.desacelerar();
+        }
+
+        private void BttnFrenar_Click(object sender, RoutedEventArgs e)
+        {
+            Frenarmsg.Text = man.frenar();
+        }
+
+        private void BttnApagar_Click(object sender, RoutedEventArgs e)
+        {
+            Apagarmsg.Text = man.apagar();
+        }
+    }
+}
